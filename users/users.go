8f9c25c0f14de18a6ae88f69df87156b84266ae1
Binyamin David <benjamin@moozen.com>
2021-09-02 14:50:00 +0300

    refactor users

--- users/users.go
+++ users/users.go
@@ -9,6 +9,37 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+func prepareToken(user *interfaces.User) string {
+	tokenContent := jwt.MapClaims{
+		"user_id": user.ID,
+		"expiry":  time.Now().Add(time.Minute * 60).Unix(),
+	}
+
+	jwtToken := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tokenContent)
+	token, err := jwtToken.SignedString([]byte("TokenPassword"))
+	helpers.HandleErr(err)
+
+	return token
+}
+
+func prepareResponse(user *interfaces.User, accounts []interfaces.ResponseAccount) map[string]interface{} {
+
+	responseUser := &interfaces.ResponseUser{
+		ID:       user.ID,
+		Username: user.Username,
+		Email:    user.Email,
+		Accounts: accounts,
+	}
+
+	var token = prepareToken(user)
+	var response = map[string]interface{}{"message": "all is ok"}
+	response["jwt"] = token
+	response["data"] = responseUser
+
+	return response
+
+}
+
 func Login(username string, password string) map[string]interface{} {
 
 	db := helpers.ConnectDB()
@@ -28,25 +59,7 @@ func Login(username string, password string) map[string]interface{} {
 	accounts := []interfaces.ResponseAccount{}
 	db.Table("accounts").Select("id, name, balance").Where("user_id = ?", user.ID).Scan(&accounts)
 
-	responseUser := &interfaces.ResponseUser{
-		ID:       user.ID,
-		Username: user.Username,
-		Email:    user.Email,
-		Accounts: accounts,
-	}
-
-	tokenContent := jwt.MapClaims{
-		"user_id": user.ID,
-		"expiry":  time.Now().Add(time.Minute * 60).Unix(),
-	}
-
-	jwtToken := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tokenContent)
-	token, err := jwtToken.SignedString([]byte("TokenPassword"))
-	helpers.HandleErr(err)
-
-	var response = map[string]interface{}{"message": "all is ok"}
-	response["jwt"] = token
-	response["data"] = responseUser
+	var response = prepareResponse(user, accounts)
 
 	return response
 

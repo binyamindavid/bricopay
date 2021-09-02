1c4e46520311db420422c49a1411ea8ba5ca2320
Binyamin David <benjamin@moozen.com>
2021-09-02 15:36:59 +0300

    create account

--- users/users.go
+++ users/users.go
@@ -41,26 +41,64 @@ func prepareResponse(user *interfaces.User, accounts []interfaces.ResponseAccoun
 }
 
 func Login(username string, password string) map[string]interface{} {
+	// Add validation to login
+	valid := helpers.Validation(
+		[]interfaces.Validation{
+			{Value: username, Valid: "username"},
+			{Value: password, Valid: "password"},
+		})
+	if valid {
+		// Connect DB
+		db := helpers.ConnectDB()
+		user := &interfaces.User{}
+		if err := db.Where("username = ? ", username).First(&user).Error; err != nil {
+			return map[string]interface{}{"message": "User not found"}
+		}
+		// Verify password
+		passErr := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
 
-	db := helpers.ConnectDB()
-	user := &interfaces.User{}
+		if passErr == bcrypt.ErrMismatchedHashAndPassword && passErr != nil {
+			return map[string]interface{}{"message": "Wrong password"}
+		}
+		// Find  user accounts
+		accounts := []interfaces.ResponseAccount{}
+		db.Table("accounts").Select("id, name, balance").Where("user_id = ? ", user.ID).Scan(&accounts)
 
-	if err := db.Where("username = ? ", username).First(&user).Error; err != nil {
-		return map[string]interface{}{"message": "User not found"}
+		var response = prepareResponse(user, accounts)
+
+		return response
+	} else {
+		return map[string]interface{}{"message": "not valid values"}
 	}
+}
 
-	// Verify Password
-	passwordErr := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+func Register(username string, email string, password string) map[string]interface{} {
+	// Add validation to registration
+	valid := helpers.Validation(
+		[]interfaces.Validation{
+			{Value: username, Valid: "username"},
+			{Value: email, Valid: "email"},
+			{Value: password, Valid: "password"},
+		})
+	if valid {
+		// Create registration logic
+		// Connect DB
+		db := helpers.ConnectDB()
+		generatedPassword := helpers.HashAndSalt([]byte(password))
+		user := &interfaces.User{Username: username, Email: email, Password: generatedPassword}
+		db.Create(&user)
 
-	if passwordErr == bcrypt.ErrMismatchedHashAndPassword && passwordErr != nil {
-		return map[string]interface{}{"message": "Wrong Password"}
+		account := &interfaces.Account{Type: "Basic Account", Name: string(username + "'s" + " account"), Balance: 0, UserID: user.ID}
+		db.Create(&account)
+
+		accounts := []interfaces.ResponseAccount{}
+		respAccount := interfaces.ResponseAccount{ID: account.ID, Name: account.Name, Balance: account.Balance}
+		accounts = append(accounts, respAccount)
+		var response = prepareResponse(user, accounts)
+
+		return response
+	} else {
+		return map[string]interface{}{"message": "not valid values"}
 	}
-
-	accounts := []interfaces.ResponseAccount{}
-	db.Table("accounts").Select("id, name, balance").Where("user_id = ?", user.ID).Scan(&accounts)
-
-	var response = prepareResponse(user, accounts)
-
-	return response
 
 }
